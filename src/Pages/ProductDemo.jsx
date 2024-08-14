595c324aed5f3e5fabcdd8f190b33c812fbff7c0
Titan <41228366+AKindWorld@users.noreply.github.com>
2024-08-14 22:37:00 +0530

    more dark mode fixes & input form small changes

--- src/Pages/ProductDemo.jsx
+++ src/Pages/ProductDemo.jsx
@@ -40,7 +40,7 @@ const ProductDemo = () => {
                 <NavBar />
             </div>
             <div>
-                <section className="bg-gray-100 h-screen">
+                <section className="h-screen">
                     <div className="mx-auto max-w-screen-xl px-4 py-28 sm:px-6 lg:px-8">
                         <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
                             <div className="lg:col-span-2 lg:py-12">
@@ -57,12 +57,12 @@ const ProductDemo = () => {
                                 </div>
                             </div>
 
-                            <div className="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
+                            <div className="rounded-lg bg-white dark:bg-[#181825] p-8 shadow-lg lg:col-span-3 lg:p-12">
                                 <form onSubmit={handleSubmit} className="space-y-4">
                                     <div>
                                         <label className="sr-only" htmlFor="name">Name</label>
                                         <input
-                                            className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                                            className="w-full rounded-lg border-gray-200 p-3 text-sm dark:bg-[#181825] focus:outline-none focus:border-[#D4C0FF] focus:ring-1"
                                             placeholder="Name"
                                             type="text"
                                             id="name"
@@ -76,7 +76,7 @@ const ProductDemo = () => {
                                         <div>
                                             <label className="sr-only" htmlFor="email">Email</label>
                                             <input
-                                                className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                                                className="w-full rounded-lg border-gray-200 p-3 text-sm dark:bg-[#181825]"
                                                 placeholder="Email address"
                                                 type="email"
                                                 id="email"
@@ -89,7 +89,7 @@ const ProductDemo = () => {
                                         <div>
                                             <label className="sr-only" htmlFor="phone">Phone</label>
                                             <input
-                                                className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                                                className="w-full rounded-lg border-gray-200 p-3 text-sm dark:bg-[#181825]"
                                                 placeholder="Phone Number"
                                                 type="tel"
                                                 id="phone"
@@ -100,11 +100,11 @@ const ProductDemo = () => {
                                         </div>
                                     </div>
 
-                                    <div className="grid grid-cols-1 gap-4 text-center sm:grid-cols-3">
+                                    <div className="grid grid-cols-1 gap-4 text-center sm:grid-cols-2">
                                         <div>
                                             <label
                                                 htmlFor="Option1"
-                                                className="block w-full cursor-pointer rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black has-[:checked]:border-black has-[:checked]:bg-black has-[:checked]:text-white"
+                                                className="block w-full cursor-pointer rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black has-[:checked]:border-[#D4C0FF] has-[:checked]:bg-[#D4C0FF] has-[:checked]:text-black has-[:checked]:font-semibold"
                                                 tabIndex="0"
                                             >
                                                 <input
@@ -125,7 +125,7 @@ const ProductDemo = () => {
                                         <div>
                                             <label
                                                 htmlFor="Option2"
-                                                className="block w-full cursor-pointer rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black has-[:checked]:border-black has-[:checked]:bg-black has-[:checked]:text-white"
+                                                className="block w-full cursor-pointer rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black has-[:checked]:border-[#D4C0FF] has-[:checked]:bg-[#D4C0FF] has-[:checked]:text-black has-[:checked]:font-semibold"
                                                 tabIndex="0"
                                             >
                                                 <input
@@ -146,7 +146,7 @@ const ProductDemo = () => {
                                         <div>
                                             <label
                                                 htmlFor="Option3"
-                                                className="block w-full cursor-pointer rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black has-[:checked]:border-black has-[:checked]:bg-black has-[:checked]:text-white"
+                                                className="block w-full cursor-pointer rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black has-[:checked]:border-[#D4C0FF] has-[:checked]:bg-[#D4C0FF] has-[:checked]:text-black has-[:checked]:font-semibold"
                                                 tabIndex="0"
                                             >
                                                 <input
@@ -167,7 +167,7 @@ const ProductDemo = () => {
                                         <div>
                                             <label
                                                 htmlFor="Option4"
-                                                className="block w-full cursor-pointer rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black has-[:checked]:border-black has-[:checked]:bg-black has-[:checked]:text-white"
+                                                className="block w-full cursor-pointer rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black has-[:checked]:border-[#D4C0FF] has-[:checked]:bg-[#D4C0FF] has-[:checked]:text-black has-[:checked]:font-semibold"
                                                 tabIndex="0"
                                             >
                                                 <input
@@ -190,7 +190,7 @@ const ProductDemo = () => {
                                         <label className="sr-only" htmlFor="message">Message</label>
 
                                         <textarea
-                                            className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                                            className="w-full rounded-lg border-gray-200 p-3 text-sm dark:bg-[#181825]"
                                             placeholder="Message"
                                             rows="8"
                                             id="message"
@@ -213,6 +213,17 @@ const ProductDemo = () => {
                         </div>
                     </div>
                 </section>
+                <div className='pink-svg-bottom-2 top-0 left-0 absolute filter blur-[500px] -z-10'>
+                <svg width="1045" height="1103" viewBox="0 0 1045 1103" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 1103V127.58C12.3484 86.5069 67.1984 3.49854 187.811 0.0518263C338.576 -4.25657 349.776 261.14 467.804 391.254C585.832 521.367 956.284 442.954 1031.24 664.406C1091.2 841.567 940.777 1030.62 858.071 1103H0Z" fill="url(#paint0_linear_679_93)"/>
+                    <defs>
+                    <linearGradient id="paint0_linear_679_93" x1="-1831.43" y1="-291.471" x2="217.086" y2="1157.86" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#FFBEDD"/>
+                    <stop offset="1" stop-color="#FF23DC"/>
+                    </linearGradient>
+                    </defs>
+                </svg>
+                </div>
             </div>
             <div>
                 <Footer />
