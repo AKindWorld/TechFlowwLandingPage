5ddd6fe50a63e0ce9c83096799e52542dd32bf89
Titan <41228366+AKindWorld@users.noreply.github.com>
2024-08-17 15:33:51 +0530

    add techstack component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,11 +3,14 @@ import '../index.css';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Services from '../components/Services';
+import TechStack from '../components/TechStack';
 import Qualities from '../components/Qualities';
 import Portfolio from '../components/PortfolioSection';
 import Testimonials from '../components/Testimonials';
 import Footer from '../components/Footer';
 import { Helmet } from 'react-helmet';
+import { useRef, useEffect } from 'react';
+
 
 const Home = () => {
     return (
@@ -21,6 +24,7 @@ const Home = () => {
             <About />
             <Services />
             <Qualities />
+            <TechStack />
             <Testimonials />
             <Footer />
             {/* <div className='bg-colors-svg'>
@@ -71,6 +75,68 @@ const Home = () => {
                 </svg>
                 </div>
             </div> */}
+            <div className='bg-colors-svg'>
+                <svg width="1600" height="900" viewBox="0 0 1600 900" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g clip-path="url(#clip0_704_29)">
+                        <g className="bg_circle" filter="url(#filter0_f_704_29)">
+                            <circle id="bgcircle" cx="330" cy="289" r="150" fill="url(#paint0_linear_704_29)"/>
+                            <circle id="bgcircle" cx="330" cy="289" r="150" stroke="#18D8D2"/>
+                        </g>
+                        <g className="bg_circle" filter="url(#filter1_f_704_29)">
+                            <rect x="970" y="600" width="403" height="403" fill="url(#paint1_linear_704_29)"/>
+                            <rect x="970" y="600" width="403" height="403" stroke="#E29016"/>
+                        </g>
+                        <g filter="url(#filter2_f_704_29)">
+                            <path d="M1190.22 362.743L1378.7 64.9816L1542.33 377.088L1190.22 362.743Z" fill="url(#paint2_linear_704_29)"/>
+                        </g>
+                        <g filter="url(#filter3_f_704_29)">
+                            <path d="M395.598 537.854C396.197 536.011 398.803 536.011 399.402 537.854L468.11 749.314C468.377 750.138 469.145 750.696 470.012 750.696H692.354C694.291 750.696 695.097 753.175 693.53 754.314L513.651 885.004C512.95 885.513 512.657 886.416 512.924 887.24L581.632 1098.7C582.231 1100.54 580.122 1102.07 578.554 1100.94L398.676 970.246C397.975 969.737 397.025 969.737 396.324 970.246L216.446 1100.94C214.878 1102.07 212.769 1100.54 213.368 1098.7L282.076 887.24C282.343 886.416 282.05 885.513 281.349 885.004L101.47 754.314C99.903 753.175 100.709 750.696 102.646 750.696H324.988C325.855 750.696 326.623 750.138 326.89 749.314L395.598 537.854Z" fill="url(#paint3_linear_704_29)"/>
+                            <path d="M395.598 537.854C396.197 536.011 398.803 536.011 399.402 537.854L468.11 749.314C468.377 750.138 469.145 750.696 470.012 750.696H692.354C694.291 750.696 695.097 753.175 693.53 754.314L513.651 885.004C512.95 885.513 512.657 886.416 512.924 887.24L581.632 1098.7C582.231 1100.54 580.122 1102.07 578.554 1100.94L398.676 970.246C397.975 969.737 397.025 969.737 396.324 970.246L216.446 1100.94C214.878 1102.07 212.769 1100.54 213.368 1098.7L282.076 887.24C282.343 886.416 282.05 885.513 281.349 885.004L101.47 754.314C99.903 753.175 100.709 750.696 102.646 750.696H324.988C325.855 750.696 326.623 750.138 326.89 749.314L395.598 537.854Z" stroke="#9B1DFF"/>
+                        </g>
+                    </g>
+                    <defs>
+                        <filter id="filter0_f_704_29" x="-320" y="-361" width="1300" height="1300" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                        <feGaussianBlur stdDeviation="250" result="effect1_foregroundBlur_704_29"/>
+                    </filter>
+                    <filter id="filter1_f_704_29" x="470" y="100" width="1403" height="1403" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                        <feGaussianBlur stdDeviation="250" result="effect1_foregroundBlur_704_29"/>
+                    </filter>
+                    <filter id="filter2_f_704_29" x="690.223" y="-435.02" width="1352.11" height="1312.11" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                        <feGaussianBlur stdDeviation="250" result="effect1_foregroundBlur_704_29"/>
+                    </filter>
+                    <filter id="filter3_f_704_29" x="-399.358" y="36.4727" width="1593.72" height="1564.86" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                        <feGaussianBlur stdDeviation="250" result="effect1_foregroundBlur_704_29"/>
+                    </filter>
+                    <linearGradient id="paint0_linear_704_29" x1="188.78" y1="139" x2="391.505" y2="207.255" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#33FF00"/>
+                        <stop offset="1" stop-color="#16D5E1"/>
+                    </linearGradient>
+                    <linearGradient id="paint1_linear_704_29" x1="1027.8" y1="506.821" x2="1428.49" y2="534.76" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#FF7A00"/>
+                        <stop offset="1" stop-color="#FFD954"/>
+                    </linearGradient>
+                    <linearGradient id="paint2_linear_704_29" x1="1520.6" y1="1224.47" x2="1672.02" y2="277.445" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#FFBEDD"/>
+                        <stop offset="1" stop-color="#FF23DC"/>
+                    </linearGradient>
+                    <linearGradient id="paint3_linear_704_29" x1="233.864" y1="532" x2="813.007" y2="696.183" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#2400FF"/>
+                        <stop offset="1" stop-color="#B923FF"/>
+                    </linearGradient>
+                    <clipPath id="clip0_704_29">
+                        <rect width="1600" height="900" fill="white"/>
+                    </clipPath>
+                    </defs>
+                </svg>
+            </div>
         </>
     )
 }
